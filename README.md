5c207539fece6159909f0feaa61a64834958e5a5
Daniil <66639134+COR72Z@users.noreply.github.com>
2022-05-20 21:00:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -7,12 +7,12 @@
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
 **Технологии, которые использовались в проекте:**
-HTML5
-CSS3
-Flexbox
-Grid
-Позиционирование
-Методология БЭМ
-JavaScript
+*HTML5*
+*CSS3*
+*Flexbox*
+*Grid*
+*Позиционирование*
+*Методология БЭМ*
+*JavaScript*
 
 * [Ссылка на проект]https://cor72z.github.io/mesto/index.html
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,144 @@
-let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__edit');
-let closePopup = popup.querySelector('.popup__close');
-let profileTitle = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#description');
+const profileEdit = document.querySelector(".profile__edit");
+const addButton = document.querySelector(".profile__add");
+const editWindow = document.querySelector(".popup_edit");
+const addWindow = document.querySelector(".popup_add");
+const buttonCloseEditPopup = document.querySelector(".popup__close_edit");
+const buttonCloseAddPopup = document.querySelector(".popup__close_add");
+const profileName = document.querySelector(".profile__name");
+const fieldEditProfileName = document.querySelector(".popup__name_edit");
+const profileJob = document.querySelector(".profile__job");
+const fieldEditJob = document.querySelector(".popup__job_edit");
+const editForm = document.querySelector(".popup__form_edit");
+const elements = document.querySelector(".elements");
+const addForm = document.querySelector(".popup__form_add");
+const inputName = document.querySelector(".popup__name_add");
+const inputLink = document.querySelector(".popup__link_add");
+const cardElement = document.querySelector("#elements").content.querySelector(".element");
+const popupOpenImage = document.querySelector(".popup_image");
+const popupImage = document.querySelector(".popup__image");
+const popupImageTitle = document.querySelector(".popup__title-image");
+const buttonClosePopupImage = document.querySelector(".popup__close_image");
+const initialCards = [
+  {
+    name: "Крым",
+    link: "https://top10a.ru/wp-content/uploads/2020/01/lastochkino_gnezdo_swallow_nest2-1shyu.jpg",
+  },
+  {
+    name: "Петергоф",
+    link: "https://kuda-spb.ru/uploads/d76af2f8fc698b0d26c7d9e46b626c34.jpg",
+  },
+  {
+    name: "Санкт-Петербург",
+    link: "https://traveltimes.ru/wp-content/uploads/2021/08/piter.jpg",
+  },
+  {
+    name: "Москва",
+    link: "https://imagesbase.ru/uploads/posts/2020-10/1601633197_imagesbase.ru.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://mwtravel.ru/wp-content/uploads/2018/02/6-Buhta-Peschanaja-Poberezhe-Bajkala.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+
+
+function closePopup(close) {
+  close.classList.remove("popup_is-active");
 }
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  togglePopup();
+function openPopup(open) {
+  open.classList.add("popup_is-active");
 }
 
-closePopup.addEventListener('click', togglePopup);
+function openEditWindow() {
+  openPopup(editWindow);
+  fieldEditProfileName.value = profileName.textContent;
+  fieldEditJob.value = profileJob.textContent;
+}
 
-formElement.addEventListener('submit', formSubmitHandler);
+function saveInfo(e) {
+  e.preventDefault();
+  profileName.textContent = fieldEditProfileName.value;
+  profileJob.textContent = fieldEditJob.value;
+  closePopup(editWindow);
+}
 
-openPopup.addEventListener('click', function () {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileJob.textContent;
-  togglePopup();
+function addFormSubmit(e) {
+  e.preventDefault();
+  elements.prepend(generateElement({ link: inputLink.value, name: inputName.value }));
+  closePopup(addWindow);
+}
+
+function like(e) {
+  e.target.classList.toggle("element__like_active");
+}
+
+function deleteClass(element) {
+  element.remove();
+}
+
+function fillPopupImage(imagePopup, titleImagePopup, card) {
+  imagePopup.src = card.link;
+  imagePopup.alt = card.name;
+  titleImagePopup.textContent = card.name;
+}
+
+function generateElement(addCard) {
+  const newElement = cardElement.cloneNode(true);
+  const cardTitle = newElement.querySelector(".element__title");
+  const cardImage = newElement.querySelector(".element__image");
+  cardImage.src = addCard.link;
+  cardImage.alt = addCard.name;
+  cardTitle.textContent = addCard.name;
+
+  const likeButton = newElement.querySelector(".element__like");
+  likeButton.addEventListener("click", like);
+
+  const deleteButton = newElement.querySelector(".element__del");
+
+  deleteButton.addEventListener("click", deleteCard);
+
+  function deleteCard() {
+    deleteClass(newElement);
+  }
+  cardImage.addEventListener("click", openPopupImage);
+
+  function openPopupImage() {
+    fillPopupImage(popupImage, popupImageTitle, addCard);
+    openPopup(popupOpenImage);
+  }
+
+  return newElement;
+}
+
+
+profileEdit.addEventListener("click", openEditWindow);
+
+buttonCloseEditPopup.addEventListener("click", () => {
+  closePopup(editWindow);
 });
 
+addButton.addEventListener("click", () => {
+  openPopup(addWindow);
+});
 
+buttonCloseAddPopup.addEventListener("click", () => {
+  closePopup(addWindow);
+});
+
+editForm.addEventListener("submit", saveInfo);
+
+addForm.addEventListener("submit", addFormSubmit);
+
+buttonClosePopupImage.addEventListener("click", () => {
+  closePopup(popupOpenImage);
+});
+
+initialCards.forEach((addCard) => {
+  elements.append(generateElement(addCard));
+});
